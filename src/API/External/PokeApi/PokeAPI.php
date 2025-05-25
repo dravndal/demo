2caa664388c68c7263014f582a679b80f80d5e6c
dravndal <danielcravndal@gmail.com>
2025-05-25 21:08:44 +0200

    feat(api): add endpoints for fetching pokemon

--- src/API/External/PokeApi/PokeAPI.php
+++ src/API/External/PokeApi/PokeAPI.php
@@ -2,23 +2,32 @@
 
 declare(strict_types=1);
 
-namespace Danielr\Demo\Src\API\External\PokeApi;
+namespace Danielr\Demo\API\External\PokeApi;
+
+use Danielr\Demo\API\Contracts\HttpClientInterface;
+use Danielr\Demo\API\Exceptions\ApiException;
 
 class PokeAPI
 {
-    const BASE_URI = 'https://pokeapi.co/api/v2/';
+    public const BASE_URI = 'https://pokeapi.co/api/v2/';
 
-    public static function getPokemonEndpoint() {}
-    public static function getPokemonTypesEndpoint() {}
-    public static function getLocationsEndpoint() {}
+    public function __construct(private HttpClientInterface $httpClient) {}
 
-    // The PokeApi does not require any authentication
-    // however in the case that an API requires auth
-    // I like to have this function because it creates a fluent
-    // and very readable API for accessing the token
-    // on a specific API by calling something like 
-    //
-    // PokeAPI::getToken();
-    //
-    public static function getToken() {}
+    public function getPokemon(string $name): array
+    {
+        try {
+            return $this->httpClient->get(self::BASE_URI . 'pokemon/' . strtolower($name));
+        } catch (\Exception $e) {
+            throw new ApiException('Failed to fetch Pokemon: ' . $e->getMessage(), 500, $e);
+        }
+    }
+
+    public function getPokemonList(int $limit = 20, int $offset = 0): array
+    {
+        try {
+            return $this->httpClient->get(self::BASE_URI . "pokemon?limit={$limit}&offset={$offset}");
+        } catch (\Exception $e) {
+            throw new ApiException('Failed to fetch Pokemon list: ' . $e->getMessage(), 500, $e);
+        }
+    }
 }
